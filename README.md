14aae35865cd721943e28994a4b13730f1af97b3
codingClaire <suyueyinyin@126.com>
2019-01-03 20:52:53 +0800

    系统性地做一个readme
    记录做题...

---
+++ README.md
@@ -0,0 +1,4 @@
+
+| ID   | Title                                    | Difficulty |                    C++                    |
+| ---- | :--------------------------------------- | :--------: | :--------------------------------------: |
+| 026  | [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/) |    Easy    | [C++](https://github.com/codingClaire/leetcode/blob/master/26.cpp) |
--- tree_tag.cpp
+++ tree_tag.cpp
@@ -107,3 +107,95 @@ public:
 		}
 	}
 };
+
+class Solution_872 {//叶子结点相同的树
+public:
+    void leafvalue(TreeNode* root,vector<int> &leafarray)
+    {
+        if(root!=NULL)
+        {
+            if(root->left==NULL&&root->right==NULL)
+            leafarray.push_back(root->val);
+            leafvalue(root->left,leafarray);
+            leafvalue(root->right,leafarray);
+    
+        }
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) 
+    {
+        vector<int> l1,l2;
+        leafvalue(root1,l1);
+        leafvalue(root2,l2);
+        if(l1.size()!=l2.size()) return false;
+        for(int i=0;i<l1.size();i++)
+        {
+            if(l1[i]!=l2[i]) return false;
+        }
+        return true;
+    }
+};
+
+class Solution_637 {//树的层次平均值
+public:
+	vector<double> averageOfLevels(TreeNode* root)
+	{
+		vector<double> aver;
+		queue<TreeNode*> q;
+		q.push(root);
+		while (!q.empty())
+		{
+			int nodesize = q.size();
+			double tmp = 0.0;
+			for (int i = 0; i<nodesize; i++)
+			{
+				tmp += q.front()->val;
+				if (q.front()->left != NULL) q.push(q.front()->left);
+				if (q.front()->right != NULL) q.push(q.front()->right);
+				q.pop();
+			}
+			aver.push_back(tmp / nodesize);
+		}
+		return aver;
+	}
+};
+
+class Solution {//判断两棵树是否相同
+public:
+    void bfs(TreeNode* root,vector<int> &tree)
+    {
+        queue<TreeNode*> qu;
+        qu.push(root);
+        while(!qu.empty())
+        {
+            int nodenum=qu.size();
+            for(int i=0;i<nodenum;i++)
+            {
+                int tmp=NULL;
+                if(qu.front()!=NULL)
+                {
+                    tmp=qu.front()->val;
+                    if(qu.front()->left!=NULL) qu.push(qu.front()->left);
+                    else qu.push(NULL);
+                    if(qu.front()->right!=NULL) qu.push(qu.front()->right);
+                    else qu.push(NULL);
+                }   
+                tree.push_back(tmp);
+                qu.pop();
+            }
+        }
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        vector<int> tree1,tree2;
+        bfs(p,tree1);
+        bfs(q,tree2);
+        if(tree1.size()!=tree2.size()) return false;
+        for(int i=0;i<tree1.size();i++)
+        {
+            if(tree1[i]!=tree2[i])
+                return false;
+        }
+        return true;
+    }
+};
